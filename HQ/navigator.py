b360d1aeba9abca81cfdad343bcd5ee96c2d0a4c
Zach Cohn <zcohn@wustl.edu>
2024-12-15 20:37:44 -0600

    working movement for explorative move

--- HQ/navigator.py
+++ HQ/navigator.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import numpy as np
 from EOS import EOS
 from gevent import sleep
 import logging
@@ -6,6 +7,7 @@ import logging
 
 class Phase(Enum):
     SETUP = "setup"
+    EXPLORE = "explore"
     OPTIMIZE = "optimize"
     COMPLETE = "complete"
     FAILED = "failed"
@@ -19,6 +21,7 @@ class Navigator:
         self.tilt = 0.0
         self.best_intensity = -1
         self.target_sensor = 1
+        self.target_sensor_previous_intensity = -1
         self.sensor_data = sensor_data
         self.lock = lock
 
@@ -39,10 +42,77 @@ class Navigator:
             self.best_intensity = self.sensor_data.get(self.target_sensor, 0)
             logging.debug(f"Setup complete. Baseline intensity: {self.best_intensity}")
 
-        return Phase.OPTIMIZE
+        return Phase.EXPLORE
+
+    def explore_phase(self):
+        # move in expanding concentric circles until the taret sensor picks up significant changes
+        logging.info("Entering EXPLORE phase.")
+
+        # Move the light in a spiral pattern
+        max_tilt = self.eos.get_tilt_range("r1")[1]
+        min_pan, max_pan = self.eos.get_pan_range("r1")
+        pan_move_step = 5
+        tilt_move_step = 5
+
+
+
+        if self.target_sensor_previous_intensity == -1:
+            self.target_sensor_previous_intensity = self.get_new_data().get(self.target_sensor, 0)
+
+        self.send_light_command(min_pan, 0, use_degrees=True)
+
+        current_intensity = self.get_new_data().get(self.target_sensor, 0)
+
+        scan_pan = min_pan
+        scan_tilt = 0
+        radius=self.tilt
+        direction = 1
+        give_up_tilt = 85
+        found = False
+        while not found:
+            # set tilt
+            for i in range(0, max_pan, pan_move_step):
+                self.eos.set_pan(1,0,scan_pan, "r1", use_degrees=True)
+                self.eos.set_tilt(1,0,scan_tilt, "r1", use_degrees=True)
+                self.pan = scan_pan
+                self.tilt = scan_tilt
+                sleep(0.05)
+                intensity = self.get_new_data().get(self.target_sensor, 0)
+
+                if intensity > self.target_sensor_previous_intensity+1000000:
+                    return Phase.COMPLETE
+
+                self.target_sensor_previous_intensity = intensity
+                scan_pan += pan_move_step * direction
+
+
+            if direction == 1 and scan_pan >= max_pan:
+                direction = -1
+                scan_pan = max_pan
+                scan_tilt += tilt_move_step
+                if scan_tilt > max_tilt:
+                    break
+            elif direction == -1 and scan_pan <= min_pan:
+                direction = 1
+                scan_pan = min_pan
+                scan_tilt += tilt_move_step
+                if scan_tilt > max_tilt:
+                    break
+            if (abs(scan_tilt) > give_up_tilt):
+                logging.info("Giving up")
+                self.eos.set_intensity(1, 0)
+                self.eos.set_pan(1, 0, 0, "r1", use_degrees=True)
+                self.eos.set_tilt(1, 0, 0, "r1", use_degrees=True)
+                return Phase.FAILED
+
+        return Phase.COMPLETE
+
+
+
+
+
 
     def optimize_phase(self):
-        sleep(5)  # Simulate optimization process
         logging.info("Entering OPTIMIZE phase.")
 
 
@@ -70,10 +140,11 @@ class Navigator:
         return new_sensor_data
 
     def execute(self):
-        logging.info(f"gui coords: {self.gui.get_sensor_positions()}")
         logging.debug(f"Executing phase: {self.current_phase}")
         if self.current_phase == Phase.SETUP:
             self.current_phase = self.setup_phase()
+        elif self.current_phase == Phase.EXPLORE:
+            self.current_phase = self.explore_phase()
         elif self.current_phase == Phase.OPTIMIZE:
             self.current_phase = self.optimize_phase()
 
